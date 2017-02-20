7134420da1ca4296a4a457823ae5f3300dce399e
acbetter <610915518@qq.com>
2017-02-20 08:46:48 +0800

    fix: this term
    新学期

--- src/schedule_to_ics.py
+++ src/schedule_to_ics.py
@@ -22,7 +22,7 @@ def get_ics(schedule):
     cal = Calendar()
     cal['version'] = '2.0'
     cal['prodid'] = '-//CQUT//Syllabus//CN'  # *mandatory elements* where the prodid can be changed, see RFC 5445
-    start_monday = date(2016, 8, 29)  # 开学第一周星期一的时间 TODO: 从 http://cale.dc.cqut.edu.cn/Index.aspx?term=2016-2017 抓取开学时间
+    start_monday = date(2017, 2, 20)  # 开学第一周星期一的时间 TODO: 从 http://cale.dc.cqut.edu.cn/Index.aspx?term=2016-2017 抓取开学时间
     dict_week = {'一': 0, '二': 1, '三': 2, '四': 3, '五': 4, '六': 5, '日': 6}
     dict_day = {1: relativedelta(hours=8, minutes=20), 3: relativedelta(hours=10, minutes=20),
                 5: relativedelta(hours=14, minutes=0), 7: relativedelta(hours=16, minutes=0),
